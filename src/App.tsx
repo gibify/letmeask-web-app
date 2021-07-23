f214a217841e242a5582db5afc352a426e3d19db
gibifyofficial <evandrogibicoski@outlook.com>
2021-07-23 20:48:26 -0300

    feat: create auth, hooks and context

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
+import { AuthContextProvider } from './context/AuthContext';
 import './styles/global.scss';
 
 function App() {
   return (
     <BrowserRouter>
+    <AuthContextProvider>
       <Route path='/' exact component={Home} />
       <Route path='/rooms/new' component={NewRoom} />
+    </AuthContextProvider>
     </BrowserRouter>
   );
 }
